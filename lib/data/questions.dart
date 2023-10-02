172cff20f8d16f66bdf64adc7bff6180cd33368c
Saeed Mardomi <27@MARDOMI-SAEED>
2023-10-02 18:17:43 +0330

    add answer screen

---
+++ lib/data/questions.dart
@@ -0,0 +1,13 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    text: 'What are the main building blocks of Flutter UIs?',
+    answers: [
+      'Widjets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  )
+];
